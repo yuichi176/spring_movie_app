16cb6d6b076c4b4b557846bb469b5d28aafe499a
Yuichi Sugiyama <ozwald176@gmail.com>
2022-05-13 09:21:02 +0900

    Movieサービスのテストクラスの追加

---
+++ src/test/java/com/example/spring_movie_app/service/MovieServiceImplTest.java
@@ -0,0 +1,47 @@
+package com.example.spring_movie_app.service;
+
+import com.example.spring_movie_app.domain.Movie;
+import com.example.spring_movie_app.repository.movie.MovieRepository;
+import com.example.spring_movie_app.service.movie.MovieServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MovieServiceImplTest {
+
+    @Mock
+    private MovieRepository movieRepository;
+
+    @BeforeEach
+    public void before() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testFindAllWhenSuccess() {
+
+        // setup
+        String input = "test";
+        Movie movie = new Movie();
+        List<Movie> result = new ArrayList<>();
+        result.add(movie);
+        Mockito.doReturn(result).when(movieRepository).findAll(input);
+        MovieServiceImpl target = new MovieServiceImpl(movieRepository);
+
+        // when
+        List<Movie> movieList = target.findAll(input);
+
+        // then
+        assertEquals(result, movieList);
+        Mockito.verify(movieRepository, Mockito.times(1)).findAll(input);
+    }
+
+
+}
